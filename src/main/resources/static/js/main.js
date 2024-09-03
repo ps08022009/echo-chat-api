7aee4cb764b473725d9d4d26670bfe0f8ea3795f
Sankar <sankpra27@issaquah.wednet.edu>
2024-09-02 20:59:18 -0700

    set default

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -7,11 +7,9 @@ var messageForm = document.querySelector('#messageForm');
 var messageInput = document.querySelector('#message');
 var messageArea = document.querySelector('#messageArea');
 var connectingElement = document.querySelector('.connecting');
-var typingIndicator = document.getElementById('typing-indicator');
 
 var stompClient = null;
 var username = null;
-var typingTimeout = null;
 
 var colors = [
     '#2196F3', '#32c787', '#00BCD4', '#ff5652',
@@ -21,60 +19,65 @@ var colors = [
 function connect(event) {
     username = document.querySelector('#name').value.trim();
 
-    if (username) {
+    if(username) {
         usernamePage.classList.add('hidden');
         chatPage.classList.remove('hidden');
 
         var socket = new SockJS('/ws');
         stompClient = Stomp.over(socket);
+
         stompClient.connect({}, onConnected, onError);
     }
-
     event.preventDefault();
 }
 
-function onConnected() {
-    stompClient.subscribe('/topic/public', onMessageReceived);
-    stompClient.subscribe('/topic/typing', onTypingReceived);
 
-    stompClient.send("/app/chat.addUser", {}, JSON.stringify({ sender: username, type: 'JOIN' }));
+function onConnected() {
+    // Subscribe to the Public Topic
+    stompClient.subscribe('/topic/public', onMessageReceived);
+
+    // Tell your username to the server
+    stompClient.send("/app/chat.addUser",
+        {},
+        JSON.stringify({sender: username, type: 'JOIN'})
+    )
 
     connectingElement.classList.add('hidden');
 }
+
 
 function onError(error) {
     connectingElement.textContent = 'Could not connect to WebSocket server. Please refresh this page to try again!';
     connectingElement.style.color = 'red';
 }
 
+
 function sendMessage(event) {
     var messageContent = messageInput.value.trim();
-
-    if (messageContent && stompClient) {
+    if(messageContent && stompClient) {
         var chatMessage = {
             sender: username,
             content: messageInput.value,
             type: 'CHAT'
         };
-
         stompClient.send("/app/chat.sendMessage", {}, JSON.stringify(chatMessage));
         messageInput.value = '';
     }
-
     event.preventDefault();
 }
 
+
 function onMessageReceived(payload) {
     var message = JSON.parse(payload.body);
-    var messageElement = document.createElement('li');
-    var displayName = (message.sender === username) ? 'Me' : message.sender;
 
-    if (message.type === 'JOIN') {
+    var messageElement = document.createElement('li');
+
+    if(message.type === 'JOIN') {
         messageElement.classList.add('event-message');
-        message.content = displayName + ' joined!';
+        message.content = message.sender + ' joined!';
     } else if (message.type === 'LEAVE') {
         messageElement.classList.add('event-message');
-        message.content = displayName + ' left!';
+        message.content = message.sender + ' left!';
     } else {
         messageElement.classList.add('chat-message');
 
@@ -82,22 +85,25 @@ function onMessageReceived(payload) {
         var avatarText = document.createTextNode(message.sender[0]);
         avatarElement.appendChild(avatarText);
         avatarElement.style['background-color'] = getAvatarColor(message.sender);
+
         messageElement.appendChild(avatarElement);
 
         var usernameElement = document.createElement('span');
-        var usernameText = document.createTextNode(displayName);
+        var usernameText = document.createTextNode(message.sender);
         usernameElement.appendChild(usernameText);
         messageElement.appendChild(usernameElement);
-
-        var textElement = document.createElement('p');
-        var messageText = document.createTextNode(message.content);
-        textElement.appendChild(messageText);
-        messageElement.appendChild(textElement);
     }
+
+    var textElement = document.createElement('p');
+    var messageText = document.createTextNode(message.content);
+    textElement.appendChild(messageText);
+
+    messageElement.appendChild(textElement);
 
     messageArea.appendChild(messageElement);
     messageArea.scrollTop = messageArea.scrollHeight;
 }
+
 
 function getAvatarColor(messageSender) {
     var hash = 0;
@@ -108,31 +114,5 @@ function getAvatarColor(messageSender) {
     return colors[index];
 }
 
-function sendTypingNotification() {
-    stompClient.send('/app/chat.typing', {}, JSON.stringify({ sender: username }));
-}
-
-function handleTypingIndicator(message) {
-    typingIndicator.innerText = `${message.body} is typing...`;
-    typingIndicator.style.display = 'block';
-    clearTimeout(typingTimeout);
-    typingTimeout = setTimeout(() => {
-        typingIndicator.style.display = 'none';
-    }, 2000);
-}
-
-function onTypingReceived(payload) {
-    var message = JSON.parse(payload.body);
-    handleTypingIndicator(message);
-}
-
-messageInput.addEventListener('input', () => {
-    sendTypingNotification();
-    clearTimeout(typingTimeout);
-    typingTimeout = setTimeout(() => {
-        typingIndicator.style.display = 'none';
-    }, 2000);
-});
-
-usernameForm.addEventListener('submit', connect, true);
-messageForm.addEventListener('submit', sendMessage, true);
+usernameForm.addEventListener('submit', connect, true)
+messageForm.addEventListener('submit', sendMessage, true)
