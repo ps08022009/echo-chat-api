006a98d7eae2bb584ddecc68bb3a99b5f139c647
Sankar <sankpra27@issaquah.wednet.edu>
2024-09-02 17:55:25 -0700

    removed files and changed css

--- message-api/message-api/src/main/java/com/pranav/message_api/MessageRepository.java
+++
@@ -1,5 +0,0 @@
-package com.pranav.message_api;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MessageRepository extends MongoRepository<Message, String> {}
